da20fddea78fe99136b271ccc18fb6794edeb7e4
Julien Croési <croesij@ensimag.grenoble-inp.fr>
2014-05-15 02:02:34 +0200

    improved version of the temperature-based heater activation algorithm

--- src/data-visualizer/filter.cpp
+++ src/data-visualizer/filter.cpp
@@ -42,6 +42,55 @@ deque<bool> *filter_heat_data(const deque<float> &src_data,
 	return out_data;
 }
 
+deque<bool> *filter_heat_data(const deque<float> &src_data,
+		float threshold_h_active, float threshold_h_inactive,
+		uint32_t inertia_h_active, int32_t inertia_h_inactive,
+		uint32_t min_count_active, uint32_t min_count_inactive)
+{
+	deque<bool> *out_data = new deque<bool>();
+	if (src_data.empty())
+		return out_data;
+	auto it = src_data.begin();
+	auto it_end = src_data.end();
+	bool curr_state = false;
+	uint32_t curr_count = 0;
+	for (; it != it_end; it++) {
+		out_data->push_back(curr_state);
+		if (curr_state) {
+			if (*it < threshold_h_inactive) {
+				if (curr_count >= min_count_inactive) {
+					curr_count = 0;
+					curr_state = false;
+					set_last_items_to(*out_data,
+							min_count_inactive
+							+ inertia_h_inactive
+							+ 1, false);
+				} else {
+					curr_count++;
+				}
+			} else {
+				curr_count = 0;
+			}
+		} else {
+			if (*it >= threshold_h_active) {
+				if (curr_count >= min_count_active) {
+					curr_count = 0;
+					curr_state = true;
+					set_last_items_to(*out_data,
+							min_count_active
+							+ inertia_h_active
+							+ 1, true);
+				} else {
+					curr_count++;
+				}
+			} else {
+				curr_count = 0;
+			}
+		}
+	}
+	return out_data;
+}
+
 deque<bool> *filter_accel_data(const deque<tuple<float, float, float>> &xyz_acc,
 		float min_var_acc, uint32_t min_count_active,
 		uint32_t min_count_inactive)
--- src/data-visualizer/filter.h
+++ src/data-visualizer/filter.h
@@ -10,6 +10,11 @@ std::deque<bool> *filter_heat_data(const std::deque<float> &src_data,
 		float threshold, uint32_t inertia_h_active,
 		uint32_t inertia_h_inactive);
 
+deque<bool> *filter_heat_data(const deque<float> &src_data,
+		float threshold_h_active, float threshold_h_inactive,
+		uint32_t inertia_h_active, int32_t inertia_h_inactive,
+		uint32_t min_count_active, uint32_t min_count_inactive);
+
 std::deque<bool> *filter_accel_data(const std::deque<std::tuple<float, float, float>> &xyz_acc,
 		float min_var_acc, uint32_t min_count_active,
 		uint32_t min_count_inactive);

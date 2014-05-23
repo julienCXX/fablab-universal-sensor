dad05fac6012a5f8ba53eeaeed22f5dd2921d9df
Julien Croési <croesij@ensimag.grenoble-inp.fr>
2014-05-23 18:13:05 +0200

    clean date handling with milliseconds in data-visualizer

---
+++ src/data-visualizer/FormattedDate.cpp
@@ -0,0 +1,64 @@
+#include "FormattedDate.h"
+
+using namespace std;
+using namespace Qt;
+
+FormattedDate::FormattedDate(const FormattedDate &other) : date(other.date)
+{
+}
+
+FormattedDate::FormattedDate(const std::string &str_date)
+{
+	setFromString(str_date);
+}
+
+bool FormattedDate::operator==(const FormattedDate &other)
+{
+	return date == other.date;
+}
+
+bool FormattedDate::operator!=(const FormattedDate &other)
+{
+	return date != other.date;
+}
+
+bool FormattedDate::operator>(const FormattedDate &other)
+{
+	return date > other.date;
+}
+
+bool FormattedDate::operator<(const FormattedDate &other)
+{
+	return date < other.date;
+}
+
+bool FormattedDate::operator>=(const FormattedDate &other)
+{
+	return date >= other.date;
+}
+
+bool FormattedDate::operator<=(const FormattedDate &other)
+{
+	return date <= other.date;
+}
+
+ostream &FormattedDate::operator<<(std::ostream &output)
+{
+	return output << toString();
+}
+
+string FormattedDate::toString()
+{
+	static const char *format = "yyyy:MM:Dd HH:mm:ss.zzz";
+	static const QString qFormat = QString(format);
+	return date.toString(qFormat).toStdString();
+}
+
+void FormattedDate::setFromString(const string &str_date)
+{
+	static const char *format = "yyyy:M:dTH:m:s.zzz";
+	static const QString qFormat = QString(format);
+	date = QDateTime::fromString(QString(str_date.c_str()), qFormat);
+}
+
+
---
+++ src/data-visualizer/FormattedDate.h
@@ -0,0 +1,49 @@
+#ifndef _H_FORMATTED_DATE
+#define _H_FORMATTED_DATE
+
+#include <string>
+#include <ostream>
+#include <QDateTime>
+
+// a simplified date format with string conversion
+class FormattedDate
+{
+	private:
+		QDateTime date;
+		// assignation operator
+		FormattedDate &operator=(const FormattedDate &fd);
+
+	public:
+		// default constructor
+		FormattedDate();
+		/*
+		 * constructor from a formatted date string
+		 * in format: "1111-12-31T23:59:59.123"
+		 */
+		FormattedDate(const std::string &str_date);
+		// copy constructor
+		FormattedDate(const FormattedDate &other);
+
+		// comparaison operators
+		bool operator==(const FormattedDate &other);
+		bool operator!=(const FormattedDate &other);
+		bool operator>(const FormattedDate &other);
+		bool operator<(const FormattedDate &other);
+		bool operator>=(const FormattedDate &other);
+		bool operator<=(const FormattedDate &other);
+
+		// flow operators
+		std::ostream &operator<<(std::ostream &output);
+
+		// prints like "1111-12-31 23:59:59.123"
+		std::string toString();
+
+		/*
+		 * setting date from a formatted date string
+		 * in format: "1111-12-31T23:59:59.123"
+		 */
+		void setFromString(const std::string &str_date);
+};
+
+#endif
+

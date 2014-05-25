6165daab810501c513805023429b725b2bedb1ac
Julien Croési <croesij@ensimag.grenoble-inp.fr>
2014-05-25 23:13:06 +0200

    full text-based settings handling

--- src/data-visualizer/Filters.h
+++ src/data-visualizer/Filters.h
@@ -7,30 +7,22 @@
 
 // filtering algorithm parameters
 typedef struct {
-	union {
-		// parameters for heat-based filters
-		struct {
-			uint32_t heatMeasureIdx;
-			union {
-				// basic filter
-				float threshold;
-				// advanced filter
-				struct {
-					float thresholdHActive;
-					float thresholdHInactive;
-				};
-			};
-			uint32_t inertiaHActive;
-			uint32_t inertiaHInactive;
-		};
-		// parameters for acceleration-based filters
-		struct {
-			uint32_t accMeasureXIdx;
-			uint32_t accMeasureYIdx;
-			uint32_t accMeasureZIdx;
-			float minVarAcc;
-		};
-	};
+	// parameters for heat-based filters
+	uint32_t inertiaHActive;
+	uint32_t inertiaHInactive;
+	uint32_t heatMeasureIdx;
+	// basic filter
+	float threshold;
+	// advanced filter
+	float thresholdHActive;
+	float thresholdHInactive;
+	// parameters for acceleration-based filters
+	uint32_t accMeasureXIdx;
+	uint32_t accMeasureYIdx;
+	uint32_t accMeasureZIdx;
+	float minVarAcc;
+	// parameter for water flow measurement
+	float flowFactor;
 	// parameters for both heat and acceleration-based filtering
 	uint32_t minCountActive;
 	uint32_t minCountInactive;
